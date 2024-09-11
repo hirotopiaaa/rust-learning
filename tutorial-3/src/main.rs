f29ee7bf1db194fb2c03dba2ef2c1ac434abeb90
hirotopiaaa <156312588+hirotopiaaa@users.noreply.github.com>
2024-09-10 22:27:17 -0400

    patch: complete tutorial-3

--- tutorial-3/src/main.rs
+++ tutorial-3/src/main.rs
@@ -1,4 +1,20 @@
 fn main() {
-    let x: u32 = 4;
+    let mut x: u32 = 4;
     println!("The value of x is: {}", x);
+    x = 5;
+    println!("The value of x is: {}", x);
+
+    // scope shadowing (interior scope)
+    {
+        let x = x - 2;
+        println!("The value of x is: {}", x);
+        let x = 2;
+        println!("The value of x is: {}", x);
+    }
+
+    let x = x + 1;
+    println!("The value of x is: {}", x);
+
+    const SECONDS_IN_MINUTE: u32 = 60;
+    println!("{}", SECONDS_IN_MINUTE);
 }
