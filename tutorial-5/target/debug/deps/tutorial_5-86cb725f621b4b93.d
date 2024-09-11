b9a54e5efe3c0f9c03161556b8533c4e2913b169
hirotopiaaa <156312588+hirotopiaaa@users.noreply.github.com>
2024-09-11 14:49:28 -0400

    feat: add tutorial-5

---
+++ tutorial-5/target/debug/deps/tutorial_5-86cb725f621b4b93.d
@@ -0,0 +1,5 @@
+/home/kev/Workspace/extras/rust-learning/tutorial-5/target/debug/deps/libtutorial_5-86cb725f621b4b93.rmeta: src/main.rs
+
+/home/kev/Workspace/extras/rust-learning/tutorial-5/target/debug/deps/tutorial_5-86cb725f621b4b93.d: src/main.rs
+
+src/main.rs:
---
+++ tutorial-5/target/debug/tutorial-5.d
@@ -0,0 +1,1 @@
+/home/kev/Workspace/extras/rust-learning/tutorial-5/target/debug/tutorial-5: /home/kev/Workspace/extras/rust-learning/tutorial-5/src/main.rs
